8103b3ce2cd9cbed0aaa57eec34f3825092b5510
NEXEO\mbertin <bertin.matthieu.pierre.hector@gmail.com>
2018-03-08 10:06:51 +0100

    add KataBankAccount source

---
+++ KataBankAccount/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataBankAccount
+{
+    public class BankAccount
+    {
+        private int _amount = 0;
+        private List<Record> _records;
+
+
+        public BankAccount()
+        {
+            _records = new List<Record>();
+        }
+
+        public int getAmount()
+        {
+            return _amount;
+        }
+
+        public List<Record> getRecord()
+        {
+            return _records;
+        }
+
+        public void deposit(int amount)
+        {
+            _amount += amount;
+            _records.Add(new Record(this, "Deposit", amount));
+        }
+
+        public void withdraw(int amount)
+        {
+            _amount -= amount;
+            _records.Add(new Record(this, "Withdraw", amount));
+        }
+
+        public string toString()
+        {
+            String str = "";
+            str += "------------------------------------------ \n";
+            str += "--\t Current Balance :\t" + _amount + "\t--\n";
+            str += "------------------------------------------ \n";
+            foreach (Record record in _records)
+            {
+                str += record.toString();
+            }
+            return str;
+        }
+    }
+}
---
+++ KataBankAccountTest/UTestKataBankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataBankAccount;
+
+namespace KataBankAccountTest
+{
+    [TestClass]
+    public class UTestKataBankAccount
+    {
+        [TestMethod]
+        public void when_deposit_of_500_account_is_increased_by_500()
+        {
+            BankAccount bankAccount = new BankAccount();
+            int oldAmount = bankAccount.getAmount();
+
+            bankAccount.deposit(500);
+
+            Assert.AreEqual(bankAccount.getAmount() - oldAmount, 500);
+        }
+
+        [TestMethod]
+        public void when_withdraw_of_500_account_is_decreased_by_500()
+        {
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.deposit(500);
+            int oldAmount = bankAccount.getAmount();
+
+            bankAccount.withdraw(500);
+
+            Assert.AreEqual(bankAccount.getAmount(), 0);
+        }
+
+        [TestMethod]
+        public void when_see_history_show_information()
+        {
+            int amount = 500;
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.deposit(amount);
+
+            String str = "";
+            str += "------------------------------------------ \n";
+            str += "--\t Current Balance :\t" + amount + "\t--\n";
+            str += "------------------------------------------ \n";
+            Record record = new Record(bankAccount, "Deposit", amount);
+            str += record.toString();
+            Assert.IsTrue(bankAccount.toString().Equals(str));
+        }
+
+    }
+}
